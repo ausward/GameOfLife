b914c05538ee572df9216f369453deacf4cc4bc3
Austin Ward <austin.albert.ward@gmail.com>
2025-07-05 21:49:31 -0600

    works, but need to work on speed and add intresting patterns

--- go/life/life.go
+++ go/life/life.go
@@ -2,6 +2,7 @@ package life
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -21,38 +22,64 @@ func (g *GameOfLifeCell) SetDead() {
 	g.Alive = false
 }
 
-
-
-
 type GameOfLifeGrid struct {
 	width  int
 	height int
 	Cells  [][]*GameOfLifeCell
+	Starts []func()
 }
 
 func NewGameOfLifeGrid(width, height int) *GameOfLifeGrid {
+
 	source := rand.NewSource(time.Now().Unix())
 	r := rand.New(source)
-	// fmt.Print(r.Intn(100))
-	
+
 	cells := make([][]*GameOfLifeCell, height)
 	for i := range cells {
 		cells[i] = make([]*GameOfLifeCell, width)
 		for j := range cells[i] {
 			cells[i][j] = &GameOfLifeCell{X: j, Y: i}
-			if (r.Int63n(500) == 1){
+			if r.Int63n(2) == 1 {
 				cells[i][j].SetAlive()
 			}
 		}
 	}
-	return &GameOfLifeGrid{
+
+	temp := &GameOfLifeGrid{
 		width:  width,
 		height: height,
 		Cells:  cells,
 	}
+	tempStarts := []func(){}
+	tempStarts = append(tempStarts, temp.NewWithCenter)
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("STERLING URGENT CARE") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("RESURGENCE") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("RESTARTING") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("REGULATING") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("ULCERATING") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("GLISTENING") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("RELUCTANCE") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("SULTRINESS") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("INCARNATE") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("INCULCATE") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("LITIGATES") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("REGULATES") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("RESCALING") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("CELL") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("START") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("RISE") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("GENES") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("NUCLEI") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("CULTURE") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("GLINT") })
+	tempStarts = append(tempStarts, func() { temp.NewGameLetters("TRUST") })
+	tempStarts = append(tempStarts, temp.NewGameRand)
+	temp.Starts = tempStarts
+
+	return temp
 }
 
-func (g *GameOfLifeGrid) getCell(x, y int) *GameOfLifeCell{
+func (g *GameOfLifeGrid) getCell(x, y int) *GameOfLifeCell {
 	if x < 0 || x >= g.width || y < 0 || y >= g.height {
 		return nil
 	}
@@ -60,19 +87,43 @@ func (g *GameOfLifeGrid) getCell(x, y int) *GameOfLifeCell{
 }
 
 func (g *GameOfLifeGrid) DoAnIteration() {
-	for  row := range g.height{
-		for col :=   range g.width {
-			// println("row :", row , "col :", col, g.DoesItLive(row, col))
 
-			if (g.DoesItLive(row,col)){
-				g.getCell(row, col).SetAlive()
+	newCells := make([][]*GameOfLifeCell, g.height)
+	for row := range newCells {
+		newCells[row] = make([]*GameOfLifeCell, g.width)
+		for col := range newCells[row] {
+			newCells[row][col] = &GameOfLifeCell{X: col, Y: row}
+		}
+	}
+	for row := range g.height {
+		for col := range g.width {
+			// println("row :", row , "col :", col, g.DoesItLive(row, col))
+			newCells[row][col].X = col
+			newCells[row][col].Y = row
+
+			if g.DoesItLive(col, row) {
+				newCells[row][col].SetAlive()
 			} else {
-				g.getCell(row,col).SetDead()
+				newCells[row][col].SetDead()
 			}
 		}
 
 	}
 
+	g.Cells = newCells
+
+}
+
+func (g *GameOfLifeGrid) NewWithCenter() {
+	centerH := g.height / 2
+	CW := g.width / 2
+	newCells := g.newEmpty()
+	newCells[centerH+1][CW].SetAlive()
+	newCells[centerH][CW].SetAlive()
+	newCells[centerH-1][CW].SetAlive()
+	newCells[centerH+1][CW+1].SetAlive()
+	newCells[centerH][CW-1].SetAlive()
+	g.Cells = newCells
 }
 
 // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
@@ -82,41 +133,216 @@ func (g *GameOfLifeGrid) DoAnIteration() {
 // _ _ _
 // _ X _
 // _ _ _
-func (g *GameOfLifeGrid) DoesItLive(xIndex, yIndex int) bool{
-	// get count of live 
-	liveNeighbors :=0
+func (g *GameOfLifeGrid) DoesItLive(col, row int) bool {
+	// get count of live
+	liveNeighbors := 0
 
 	up := -1
 	down := 2
 	left := -1
 	right := 2
 
-	if (xIndex == 0){
-		up = 0
-	} else if (xIndex == g.height -1 ){ down =1 }
+	// Corrected border handling for xIndex (column)
+	if col == 0 {
+		left = 0 // Adjust 'left' bound for cells in the first column
+	} else if col == g.width-1 {
+		right = 1 // Adjust 'right' bound for cells in the last column
+	}
 
-	if (yIndex ==0){left = 0} else if (yIndex == g.width -1 ){right = 1}
-	
-	
+	// Corrected border handling for yIndex (row)
+	if row == 0 {
+		up = 0 // Adjust 'up' bound for cells in the first row
+	} else if row == g.height-1 {
+		down = 1 // Adjust 'down' bound for cells in the last row
+	}
 
 	for i := up; i < down; i++ {
-		for j:=left;j<right;j++{
-			if (g.Cells[xIndex+i][yIndex+j].Alive){
-			liveNeighbors++
+		for j := left; j < right; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if g.Cells[row+i][col+j].Alive {
+				liveNeighbors++
 			}
 		}
 	}
 
-	if (!g.getCell(xIndex, yIndex).IsAlive() && liveNeighbors==3){return true}
+	if !g.getCell(col, row).IsAlive() && liveNeighbors == 3 {
+		return true
+	}
 
-	switch liveNeighbors{
-		case 0, 1:
-			return false;
-		case 2, 3:
-			return true;
-		default:
-			return false;
+	if g.getCell(col, row).IsAlive() {
+		// Rules for a live cell
+		if liveNeighbors < 2 || liveNeighbors > 3 {
+			return false // Underpopulation or Overpopulation
+		}
+		return true // Survival
+	} else {
+		// Rule for a dead cell
+		if liveNeighbors == 3 {
+			return true // Reproduction
+		}
+		return false // Remains dead
 	}
 
 }
 
+func (g *GameOfLifeGrid) newEmpty() [][]*GameOfLifeCell {
+	newCells := make([][]*GameOfLifeCell, g.height)
+	for i := range newCells {
+		newCells[i] = make([]*GameOfLifeCell, g.width)
+		for j := range newCells[i] {
+			newCells[i][j] = &GameOfLifeCell{X: j, Y: i}
+		}
+	}
+
+	return newCells
+}
+
+// same as whats in the iterator
+func (g *GameOfLifeGrid) NewGameRand() {
+	source := rand.NewSource(time.Now().Unix())
+	r := rand.New(source)
+
+	cells := make([][]*GameOfLifeCell, g.height)
+	for i := range cells {
+		cells[i] = make([]*GameOfLifeCell, g.width)
+		for j := range cells[i] {
+			cells[i][j] = &GameOfLifeCell{X: j, Y: i}
+			if r.Int63n(2) == 1 {
+				cells[i][j].SetAlive()
+			}
+		}
+	}
+
+	g.Cells = cells
+}
+
+func (g *GameOfLifeGrid) NewGameLetters(message string) {
+
+	centerRow := (g.height / 3)
+	centerCol := (g.width / 2) - 25
+
+	newCells := g.newEmpty()
+
+	UpperCase_massage := strings.ToUpper(message)
+
+	for _, r := range UpperCase_massage {
+
+		if r == ' ' {
+			centerRow += 7
+			centerCol -= 10
+			continue
+		}
+
+		let, ok := letters[r]
+		if !ok {
+			g.NewWithCenter()
+			return
+		}
+
+		for row := range 5 {
+			for col := range 3 {
+				if let[row][col] {
+					newCells[centerRow+row][centerCol+col].SetAlive()
+				}
+			}
+		}
+
+		centerCol += 5
+	}
+
+	g.Cells = newCells
+}
+
+// slice of functions
+
+// Time For Letters
+
+var letters = map[rune][5][3]bool{
+	'S': { // 'S' is the rune key
+		{true, true, true},   // This is row 0 (3 boolean values)
+		{true, false, false}, // This is row 1
+		{true, true, true},   // This is row 2
+		{false, false, true}, // This is row 3
+		{true, true, true},   // This is row 4
+	},
+	'T': {
+		{true, true, true},
+		{false, true, false},
+		{false, true, false},
+		{false, true, false},
+		{false, true, false},
+	},
+	'E': {
+		{true, true, true},
+		{true, false, false},
+		{true, true, true},
+		{true, false, false},
+		{true, true, true},
+	},
+	'R': {
+		{true, true, true},
+		{true, false, true},
+		{true, true, true},
+		{true, true, false},
+		{true, false, true},
+	},
+	'L': {
+		{true, false, false},
+		{true, false, false},
+		{true, false, false},
+		{true, false, false},
+		{true, true, true},
+	},
+	'I': {
+		{true, true, true},
+		{false, true, false},
+		{false, true, false},
+		{false, true, false},
+		{true, true, true},
+	},
+	'N': {
+		{true, true, true},
+		{true, false, true},
+		{true, false, true},
+		{true, false, true},
+		{true, false, true},
+	},
+	'G': {
+		{false, true, true},
+		{true, false, false},
+		{true, false, true},
+		{true, false, true},
+		{false, true, false},
+	},
+	'U': {
+		{true, false, true},
+		{true, false, true},
+		{true, false, true},
+		{true, false, true},
+		{false, true, false},
+	},
+	'C': {
+		{false, true, false},
+		{true, false, true},
+		{true, false, false},
+		{true, false, true},
+		{false, true, false},
+	},
+	'A': {
+		{false, true, false},
+		{true, false, true},
+		{true, true, true},
+		{true, false, true},
+		{true, false, true},
+	},
+	// ... more characters
+	' ': { // The space character
+		{false, false, false},
+		{false, false, false},
+		{false, false, false},
+		{false, false, false},
+		{false, false, false},
+	},
+}
--- go/main.go
+++ go/main.go
@@ -6,7 +6,7 @@ import (
 
 	// "strconv"
 
-	// "time"
+	"time"
 
 	"github.com/auswar/GameOfLife/life"
 
@@ -16,30 +16,70 @@ import (
 )
 
 const (
-	screenHeight int = 800
-	screenWidth  int = 800
+	screenHeight int = 900
+	screenWidth  int = 1900
 )
 
 type Game struct {
-	Width    int
-	Height   int
-	cellSize int
-	life     *life.GameOfLifeGrid
+	Width      int
+	Height     int
+	cellSize   int
+	life       *life.GameOfLifeGrid
+	TIPS       int
+	Time       time.Time
+	R          int
+	G          int
+	B          int
+	StartIndex int
 }
 
 func (g *Game) Update() error {
-	// time.Sleep(1 * time.Second)
+	ebiten.SetTPS(g.TIPS)
 	g.life.DoAnIteration()
+
+	// if (g.R <= 2){g.R,g.G,g.B = 255, 255, 255} else {
+	// g.R -= 1
+	g.B -= 2
+	g.G += 3
+	// }
+	// if ebiten.IsKeyPressed(ebiten.KeyM) {
+	// 	g.life.NewGameLetters("STERLING URGENT CARE")
+	// }
+	if ebiten.IsKeyPressed(ebiten.KeyN) {
+		g.Time = time.Now().Add(-20 * time.Hour)
+		
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyS) && g.TIPS > 1 {
+		g.TIPS--
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyF) {
+		g.TIPS++
+	}
+
+	if time.Now().After(g.Time.Add(4 * time.Minute)) {
+		g.Time = time.Now()
+		tempIndex := g.StartIndex
+		if g.StartIndex > len(g.life.Starts)-1 {
+			tempIndex = 0
+			g.StartIndex = 0
+		} else {
+			g.StartIndex++
+		}
+		ebiten.SetTPS(1)
+		g.life.Starts[tempIndex]()
+	}
+	// g.TIPS++
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	// ebitenutil.DebugPrint(screen, "Hello, World!")
-	
+
 	for _, x := range g.life.Cells {
 		for _, y := range x {
 			if y.IsAlive() {
-				vector.DrawFilledRect(screen, float32(y.X*g.cellSize), float32(y.Y*g.cellSize), float32(g.cellSize), float32(g.cellSize), color.RGBA{255, 255, 255, 255}, true)
+				vector.DrawFilledRect(screen, float32(y.X*g.cellSize), float32(y.Y*g.cellSize), float32(g.cellSize), float32(g.cellSize), color.RGBA{uint8(g.R), uint8(g.G), uint8(g.B), 255}, true)
 			}
 
 		}
@@ -53,14 +93,18 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	cellSize := 1
-	conn := life.NewGameOfLifeGrid(800/cellSize, 800/cellSize)
+	// the smaller the size the more the cells
+	cellSize := 8
+
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Hello, World!")
-	ebiten.NewImage(100, 100).Fill(color.White)
-	ebiten.SetTPS(10)	
-	// ebiten.SetVsyncEnabled(false)
-	if err := ebiten.RunGame(&Game{Width: 800, Height: 800, cellSize: cellSize, life: conn}); err != nil {
+	ebiten.SetWindowTitle("Go Way Of Life")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	ebiten.MaximizeWindow()
+	ebiten.SetFullscreen(true)
+	windowWidth, windowHeight := ebiten.Monitor().Size()
+	conn := life.NewGameOfLifeGrid(windowWidth/cellSize, windowHeight/cellSize)
+	ebiten.SetWindowSize(windowWidth, windowHeight)
+	if err := ebiten.RunGame(&Game{Width: windowWidth, Height: windowHeight, cellSize: cellSize, life: conn, TIPS: 8, Time: time.Now(), R: 255, G: 255, B: 255, StartIndex: 0}); err != nil {
 		log.Fatal(err)
 	}
 }
